cdbfe15b97067fba4b8a385debbf67e4f1cf19e3
hassaniko <elsawyhassan2@gmail.com>
2021-12-25 23:34:04 +0200

    basic random array generation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 
+import java.util.Random;
+import static java.lang.Math.abs;
+
 public class Main {
 
+    static int [] randArray(int size){
+        int [] array = new int[size];
+        Random rd = new Random();
+        for(int i = 0; i < size; i++)
+            array[i] = rd.nextInt();
+        return array;
+
+    }
+
+    static void printArrayOfInt(int[] arr, int size){
+        for(int i = 0; i<size ; i++)
+            System.out.println(arr[i]);
+    }
+
+    static int randomizedPivot(int[] arr, int size){
+        Random rd = new Random();
+        return abs(rd.nextInt() % size);
+    }
+
     public static void main(String[] args) {
-
-        System.out.print("hello World!");
-
+        int size = 10;
+        int [] arr = randArray(size);
+        //printArrayOfInt(arr, size);
+        System.out.println(randomizedPivot(arr, size));
     }
 }
